e26245dbe71ebee35ab325dd62345a0ec9e14b32
Hing <jiangzhengxing@zbj.com>
2016-08-09 14:09:13 +0800

    something change

--- themes/hexo-theme-next/source/js/src/my.js
+++ themes/hexo-theme-next/source/js/src/my.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
     $('.sidebar-toggle').trigger('click');
-    $('#sidebar').find('li').each(function() {
-        if ($(this).hasClass('sidebar-nav-active') == false) {
-            $(this).trigger('click');
-        }
-    });
-
 });
